6db0253f4156507278a67160faef7433c3c92229
juliensoret97 <j.soret@rt-iut.re>
2021-04-26 11:46:09 +0400

    style + CGU + img

--- src/Form/ChoixType.php
+++ src/Form/ChoixType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Choix;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class ChoixType extends AbstractType
 {
@@ -21,7 +24,19 @@ class ChoixType extends AbstractType
                 'CCP Secrétaire comptable' => 'CCP Secrétaire comptable',
                 'CCP ARH'=>'CCP ARH'
             ],'multiple'=>false, 'attr' => ['class' => 'form-control']])
-        ;
+            ->add('accepter', CheckboxType::class, ['mapped' => false,
+                // 'label' => "<a href='{{asset ('pdf/politique.pdf')}}'>Politique de conf</a>",
+            ])
+            ->addEventListener(FormEvents::PRE_SUBMIT, function (
+                FormEvent $event
+            ) {
+                $personne = $event->getData();
+                if (!isset($personne['accepter']) || !$personne['accepter']) {
+                    exit();
+                }
+            });
+       
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
